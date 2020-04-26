afb0553d77a25942c9d898619d8049c79021277c
mastermindapple <56500380+mastermindapple@users.noreply.github.com>
2020-04-26 20:29:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var preference = undefined;
+var preference;
 function data(){
-    preference = "Data Handling";
+    preference = "Crop";
     console.log(preference);
+    document.getElementById("choice").innerHTML = ("Your preferable chapter is Crop production and management");
 }
 function root(){
-    preference = "Square Root";
+    preference = "Microorganisms";
     console.log(preference);
+    document.getElementById("choice").innerHTML = ("Your preferable chapter is Microorganisms: Friend and Foe");
 }
 function quad(){
-    preference = "Understanding Quadrilaterals";
+    preference = "Metals";
     console.log(preference);
-
+    document.getElementById("choice").innerHTML = ("Your preferable chapter is Metals and Non-Metals");
 }
 
 var button = document.getElementById("submit");
@@ -25,19 +27,24 @@ button.addEventListener("click",(e) =>{
         alert("Please enter your name to proceed");
     }
     
-    else if(!preference){
+    else if(preference === undefined){
         alert("Please select a chapter");
     }
     else{
      database.ref("/Users/"+name).set({
             Chapter: preference
         });
-        document.getElementById("header").innerHTML = ("Choice Submitted!");
-        document.getElementById("input").innerHTML = ("");
-        document.getElementById("options").innerHTML = ("Results will be out in the next Class");
-        document.getElementById("btn").innerHTML = ("");
+        setTimeout(giveConfirmation,3000);
     }
 });
+function giveConfirmation(){
+    document.getElementById("header").innerHTML = ("Choice Submitted!");
+    document.getElementById("input").innerHTML = ("");
+    document.getElementById("options").innerHTML = ("Results will be out in the next Class");
+    document.getElementById("btn").innerHTML = ("");
+    document.getElementById("choice").innerHTML = ("");
+
+}
 
 
  
